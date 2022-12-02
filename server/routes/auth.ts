8604a87da4a752797140b29e5398b203e6bb8e74
Alex Alikhani <alikhanialex@gmail.com>
2022-12-02 08:21:01 +0000

    Validating the signup email and password using express-validator

--- server/routes/auth.ts
+++ server/routes/auth.ts
@@ -1,14 +1,31 @@
 import express from "express";
+import { body, validationResult } from "express-validator";
 
 const router = express.Router();
 
-router.post("/signup", async (req, res) => {
-  const { email, password } = req.body;
+router.post(
+  "/signup",
+  body("email").isEmail().withMessage("This email is invalid"), // Check that an email has been entered in the body and it is valid
+  body("password").isLength({ min: 5 }).withMessage("Password must be at least 5 characters"), // Check password is at least 5 characters
+  async (req, res) => {
+    const validationErrors = validationResult(req); // This returns an array of errors
 
-  res.json({
-    email,
-    password,
-  });
-});
+    if (!validationErrors.isEmpty()) {
+      const errors = validationErrors.array().map((error) => {
+        return {
+          msg: error.msg,
+        };
+      });
+      return res.json({ errors });
+    }
+
+    const { email, password } = req.body;
+
+    res.json({
+      email,
+      password,
+    });
+  }
+);
 
 export default router;
