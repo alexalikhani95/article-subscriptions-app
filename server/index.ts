cd090bd2c265807ca5755da0fd9c5926cfaadd66
Alex Alikhani <alikhanialex@gmail.com>
2022-12-02 08:11:37 +0000

    Creating a basic signup route

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,12 @@
 import express from "express";
+import authRoutes from "./routes/auth";
 
 const app = express();
+
+app.use(express.json()); // express. json() is a built in middleware function in Express starting from v4. 16.0. It parses incoming JSON requests and puts the parsed data in req.
+app.use("/auth", authRoutes);
+
+app.get("/", (req, res) => res.send("hello"));
 
 app.listen(5000, () => {
   console.log(`Server listening on port 5000`);
---
+++ server/routes/auth.ts
@@ -0,0 +1,14 @@
+import express from "express";
+
+const router = express.Router();
+
+router.post("/signup", async (req, res) => {
+  const { email, password } = req.body;
+
+  res.json({
+    email,
+    password,
+  });
+});
+
+export default router;
