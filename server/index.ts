84cda087bedeb4a2379e4cd27dda3e5e66176c31
Alex Alikhani <alikhanialex@gmail.com>
2022-12-04 14:00:02 +0000

    Installing cors to prevent potential cors issues

--- server/index.ts
+++ server/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import authRoutes from "./routes/auth";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import cors from "cors";
 
 dotenv.config();
 const PORT = process.env.port;
@@ -9,6 +10,7 @@ const PORT = process.env.port;
 const app = express();
 
 app.use(express.json()); // express. json() is a built in middleware function in Express starting from v4. 16.0. It parses incoming JSON requests and puts the parsed data in req.
+app.use(cors());
 app.use("/auth", authRoutes);
 
 mongoose
