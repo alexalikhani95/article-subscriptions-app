c18785a3351bf7dd9405d0b0974a468f5dfcfd25
Alex Alikhani <alikhanialex@gmail.com>
2022-12-05 08:29:30 +0000

    Show a logout tab in the navbar if the user is logged in (if there is a token in localstorage)

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -4,7 +4,14 @@ import { Link } from "react-router-dom";
 const NavBar = () => {
   return (
     <div style={{ borderBottom: "1px solid blue", display: "flex" }}>
-      <Link to="/">Home</Link>
+      <div style={{ margin: "20px" }}>
+        <Link to="/">Home</Link>
+      </div>
+      {localStorage.getItem("token") && (
+        <div style={{ margin: "20px" }}>
+          <Link to="/">Logout</Link>
+        </div>
+      )}
     </div>
   );
 };
