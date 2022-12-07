5f2e4714d576676e1089e9751af530551ca05567
Alex Alikhani <alikhanialex@gmail.com>
2022-12-07 20:10:49 +0000

    Logout in navbar now removes token from localstrorage and resets user state and navigates to the homepage

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,17 +1,26 @@
 import React, { useContext } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { UserContext } from "../context";
 
 const NavBar = () => {
   const [state, setState] = useContext(UserContext);
+
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    setState({ data: null, loading: false, error: null });
+    localStorage.removeItem("token");
+    navigate("/");
+  };
+
   return (
     <div style={{ borderBottom: "1px solid blue", display: "flex" }}>
       <div style={{ margin: "20px" }}>
         <Link to="/">Home</Link>
       </div>
       {state.data && ( // If there is something in localstorage (token in this case) show the logout
-        <div style={{ margin: "20px" }}>
-          <Link to="/">Logout</Link>
+        <div style={{ margin: "20px", cursor: "pointer" }}>
+          <div onClick={handleLogout}>Logout</div>
         </div>
       )}
     </div>
