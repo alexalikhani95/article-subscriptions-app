bc7a8ae1a689a97941c7ee400ccc861ecb162eb0
Alex Alikhani <alikhanialex@gmail.com>
2022-12-07 21:56:59 +0000

    Creating a subs route for subscriptions and Adding stripe keys to env file (file not included in commit)

--- server/index.ts
+++ server/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import authRoutes from "./routes/auth";
+import subsRoutes from "./routes/subs";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import cors from "cors";
@@ -12,6 +13,7 @@ const app = express();
 app.use(express.json()); // express. json() is a built in middleware function in Express starting from v4. 16.0. It parses incoming JSON requests and puts the parsed data in req.
 app.use(cors());
 app.use("/auth", authRoutes);
+app.use("/subs", subsRoutes);
 
 mongoose
   .connect(process.env.MONGO_URI as string)
---
+++ server/routes/subs.ts
@@ -0,0 +1,10 @@
+import express from "express";
+import { checkAuth } from "../middleware/checkAuth";
+
+const router = express.Router();
+
+router.get("/prices", checkAuth, async (req, res) => {
+  res.send("prices");
+});
+
+export default router;
