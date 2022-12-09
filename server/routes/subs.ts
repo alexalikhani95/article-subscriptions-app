34bf44c35485246a6f524a1eec879e508a2e3546
Alex Alikhani <alikhanialex@gmail.com>
2022-12-09 08:44:37 +0000

    Creating an Article model and manually creating artciles for basic, standard and premium to get them in the database

--- server/routes/subs.ts
+++ server/routes/subs.ts
@@ -1,7 +1,9 @@
 import express from "express";
 import { checkAuth } from "../middleware/checkAuth";
+import article from "../models/article";
 import User from "../models/User";
 import { stripe } from "../utils/stripe";
+import Article from "../models/article";
 
 const router = express.Router();
 
@@ -14,6 +16,15 @@ router.get("/prices", checkAuth, async (req, res) => {
 
 router.post("/session", checkAuth, async (req, res) => {
   const user = await User.findOne({ email: req.user });
+
+  Article.create({
+    title: "Zebras",
+    imageUrl:
+      "https://images.unsplash.com/photo-1666723655505-419fd684c451?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDM3fEpwZzZLaWRsLUhrfHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur",
+    access: "Basic",
+  });
 
   const session = await stripe.checkout.sessions.create(
     {
