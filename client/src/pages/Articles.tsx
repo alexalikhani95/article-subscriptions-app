c472c0001eac134f6879d47728ab221ce42f1e7a
Alex Alikhani <alikhanialex@gmail.com>
2022-12-10 18:25:54 +0000

    Creating Articles component which displays the articles that a user has access to

--- client/src/pages/Articles.tsx
+++ client/src/pages/Articles.tsx
@@ -1,7 +1,63 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+
+interface Article {
+  id: string;
+  title: string;
+  imageUrl: string;
+  content: string;
+}
 
 const Articles = () => {
-  return <div>Articles</div>;
+  const [articles, setArticles] = useState<Article[]>([]); // The type of state is an array of type Article
+
+  const fetchArticles = async () => {
+    const { data: response } = await axios.get("http://localhost:5001/articles");
+    setArticles(response);
+  };
+
+  useEffect(() => {
+    fetchArticles();
+  });
+
+  return (
+    <div>
+      {articles.length ? (
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          {articles.map((article) => (
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                width: 400,
+                border: "2px solid black",
+                marginBottom: 20,
+              }}
+            >
+              {article.imageUrl && (
+                <img src={article.imageUrl} alt="Article" style={{ height: 200, width: 200 }} />
+              )}
+              <h3>{article.title}</h3>
+              <p>{article.content}</p>
+            </div>
+          ))}
+        </div>
+      ) : (
+        <div style={{ display: "flex", flexDirection: "column" }}>
+          <h2>No articles</h2>
+          <Link to="/">Buy a subscription plan</Link>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Articles;
